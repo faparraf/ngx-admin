f69b990761554cfa8eaf10cb140a8dea0c4dcb88
Fabián León <fadarsaleeing@gmail.com>
2018-01-28 16:28:11 -0500

    organizacion

---
+++ src/app/@core/data/clust.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
+const path = 'https://riopiwbvwh.execute-api.us-east-1.amazonaws.com/Test';
+
+@Injectable()
+export class ClustService {
+
+    constructor(private http:HttpClient) {
+    }
+    get() {
+        return this.http.get('/api/food');
+    }
+    post(endpoint, element) {
+        const body = JSON.stringify(element);
+        return this.http.post(path + endpoint, body, httpOptions);
+    }
+    put(endpoint, element) {
+        const body = JSON.stringify(element);
+        return this.http.put(path + endpoint + element.id, body, httpOptions);
+    }
+    delete(endpoint, element) {
+        return this.http.delete(path + endpoint + element.id);
+    }
+};
