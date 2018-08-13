b739ad205ec4f834f17203718c4524643f3a3e2a
Fabián León <fadarsaleeing@gmail.com>
2018-08-12 23:37:41 -0500

    lint

--- src/app/@core/data/apolo.service.ts
+++ src/app/@core/data/apolo.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {  HttpHeaders } from '@angular/common/http';
 import { Config } from './../../app-config'
 import { Apollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular-link-http';
@@ -9,11 +9,11 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
-
+@Injectable()
 class ApoloComponent {
     constructor(apollo: Apollo, httpLink: HttpLink) {
         apollo.create({
-            link: httpLink.create({ uri: 'https://api.example.com/graphql' }),
+            link: httpLink.create({ uri: Config.LOCAL.GRAPHQL }),
             cache: new InMemoryCache(),
         });
     }
